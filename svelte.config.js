04075e3e35a34ecfe9f24ba18c11fbac6ccc648a
s-javier <javiersalamank@gmail.com>
2025-04-24 11:05:58 -0400

    Wip

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-node'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 const config = {
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
-};
+  preprocess: vitePreprocess(),
+  kit: { adapter: adapter(), alias: { '~/*': './src/*' } },
+}
 
-export default config;
+export default config
