2a8f30291fafde0a6b57830bdd69e2875b5a8c0d
s-javier <javiersalamank@gmail.com>
2025-05-02 00:16:42 -0400

    Wip

--- src/routes/(auth)/ingreso/+page.server.ts
+++ src/routes/(auth)/ingreso/+page.server.ts
@@ -1,9 +1,14 @@
 import { type RequestEvent, redirect } from '@sveltejs/kit'
 import { eq } from 'drizzle-orm'
-import { NODE_ENV } from '$env/static/private'
+import { NODE_ENV, SESSION_DAYS } from '$env/static/private'
 import db from '~/lib/server/db'
-import { personTable } from '~/lib/server/db/schema'
+import { personTable, sessionTable } from '~/lib/server/db/schema'
 import rollbar from '~/lib/server/rollbar'
+import { customAlphabet } from 'nanoid'
+import { add } from 'date-fns'
+// @ts-ignore
+import { createTransport } from 'nodemailer'
+import { General } from '~/enums'
 
 type Error = {
   server?: string
@@ -14,9 +19,14 @@ class Machine {
   email: string
   error: Error = {}
   user: any
+  code: string = ''
 
   constructor(email: string) {
     this.email = email
+  }
+
+  getIsError() {
+    return Object.keys(this.error).length > 0
   }
 
   validateForm() {
@@ -38,7 +48,7 @@ class Machine {
         .from(personTable)
         .where(eq(personTable.email, this.email))
     } catch (e: any) {
-      rollbar.error('En "/ingreso", error al consultar por email en db.', e)
+      rollbar.error('Error en DB. Ruta "/ingreso". Consulta de email.', e)
       this.error.server = 'Hubo un error. Por favor, inténtalo de nuevo o más tarde.'
       throw new Error()
     }
@@ -49,6 +59,70 @@ class Machine {
       return
     }
     this.user = query[0]
+  }
+
+  getUserIsActtive() {
+    if (this.user.isActive === false) {
+      if (NODE_ENV === 'development') {
+        console.error('Usuario no activo.')
+      }
+      throw new Error()
+    }
+  }
+
+  async createSession() {
+    const alphabet = '0123456789'
+    this.code = customAlphabet(alphabet, 6)()
+    const expiresAt = add(new Date(), { days: Number(SESSION_DAYS) })
+    const codeExpiresAt = add(new Date(), { minutes: 5 })
+    try {
+      await db.insert(sessionTable).values({
+        personId: this.user.id,
+        expiresAt,
+        code: this.code,
+        codeExpiresAt,
+        codeIsActive: true,
+      })
+    } catch (e: any) {
+      rollbar.error('Error de DB. Ruta "/ingreso". Crear sesión.', e)
+      this.error.server = 'Hubo un error. Por favor, inténtalo de nuevo o más tarde.'
+      throw new Error()
+    }
+  }
+
+  async sendEmail() {
+    const transporter = createTransport({
+      host: process.env.SMTP_HOST,
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+      tls: { rejectUnauthorized: false },
+      debug: true,
+    })
+    try {
+      await transporter.sendMail({
+        from: `"${General.TITLE}" <noresponder@condimento.cl>`,
+        to: this.email,
+        subject: 'Ingreso',
+        html: `
+          <div style="background-color:rgb(207,208,209);padding-top:30px;padding-bottom:30px">
+            <div style="padding:30px;font-size:14px;font-family:Lato,Helvetica,Arial,sans-serif;color:rgb(55,65,81);line-height:1.5em;width:98%;max-width:500px;border-radius:16px;margin:10px auto 0;background-color:white">
+              <p style="margin-bottom: 16px">Hola, ${this.user.firstName}:</p>
+              <p style="margin-bottom: 16px">Bienvenido/a a ${General.TITLE}. Por favor, utiliza este código para ingresar:</p>
+              <p style="margin-bottom: 30px; text-align: center;">${this.code}</p>
+              <p>Que tengas un buen día.</p>
+            </div>
+          </div>
+        `,
+      })
+    } catch (e: any) {
+      rollbar.error('Error de envío de email. Ruta "/ingreso".', e)
+      this.error.server = 'Hubo un error. Por favor, inténtalo de nuevo o más tarde.'
+      throw new Error()
+    }
   }
 }
 
@@ -62,10 +136,14 @@ export const actions = {
     try {
       machine.validateForm()
       await machine.getUser()
-    } catch {
+      machine.getUserIsActtive()
+      await machine.createSession()
+      await machine.sendEmail()
+    } catch {}
+
+    if (machine.getIsError()) {
       return { error: machine.error }
     }
-
     event.cookies.set('login', 'true', { path: '/' })
     redirect(303, '/codigo')
   },
